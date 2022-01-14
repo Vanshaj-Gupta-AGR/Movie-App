5374f92c8c0d181d343e4123a6662c670dbdd3e7
Vanshaj Gupta <vanshaj444@gmail.com>
2022-01-14 15:36:07 +0530

    reducres started

---
+++ src/components/MovieCard.js
@@ -0,0 +1,41 @@
+import React from "react";
+
+
+
+class MovieCard extends React.Component {
+
+
+    render(){
+        const { movie }=this.props;
+        console.log("gbhgb")
+  return (
+    <div className="movie-card">
+        <div className="left">
+            <img alt="movie-poster" src={movie.Poster} />
+        </div>
+        <div className="right">
+
+        <div className="title">{movie.Title}</div>
+        <div className="plot">{movie.Plot}</div>
+        <div className="footer">
+            <div className="rating">
+                {this.props.movie.imdbRating}
+            </div>
+            <button className="favourite-btn">
+                Favourite
+            </button>
+        </div>
+        
+
+        </div>
+
+        
+         
+        </div>
+     
+    
+  );
+}
+}
+
+export default MovieCard;
