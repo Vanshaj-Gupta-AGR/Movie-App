bb08b2e21dca94db4c11316f9cefc811e948aa5e
Vanshaj Gupta <vanshaj444@gmail.com>
2022-01-16 14:30:53 +0530

    subscribe

--- src/components/App.js
+++ src/components/App.js
@@ -1,26 +1,41 @@
 import Navbar from './Navbar';
 import MovieCard from './MovieCard';
 import React from 'react';
+import {data } from '../data'
 
 class App extends React.Component{
 
+  componentDidMount(){
+
+    const {store}=this.props
+    store.subscribe(()=>{
+      console.log("updated")
+      this.forceUpdate();
+    })
+
+    store.dispatch({
+      type: "ADD_MOVIES",
+      movies: data
+    })
+  }
+
   render(){
-  const movies= this.props.store.getState();
-  return (
-    <div className="App">
-      <Navbar />
-      <div className="main">
-        <div className="tabs">
-          <div className="tab">Movie</div>
-          <div className="tab">Favourites</div>
-        </div>
-        <div className="list">
-          {movies.map((movie,index) =>{
-            return <MovieCard movie={movie} key={`movies-${index}`} />
-          })}
+    const movies= this.props.store.getState();
+    return (
+      <div className="App">
+        <Navbar />
+        <div className="main">
+          <div className="tabs">
+            <div className="tab">Movie</div>
+            <div className="tab">Favourites</div>
+          </div>
+          <div className="list">
+            {movies.map((movie,index) =>{
+              return <MovieCard movie={movie} key={`movies-${index}`} />
+            })}
+          </div>
         </div>
       </div>
-    </div>
   );
 }
 }
